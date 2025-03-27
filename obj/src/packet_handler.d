e8482d2c124c616a8c1cec5c8bd704e4026e05e2
erdogancayir <erdogancayir98@gmail.com>
2025-03-27 22:41:34 +0300

    make fclean

--- obj/src/packet_handler.d
+++
@@ -1,1 +0,0 @@
-obj/src/packet_handler.o: src/packet_handler.c
--- obj/src/pcap/add_scan_result.d
+++
@@ -1,5 +0,0 @@
-obj/src/pcap/add_scan_result.o: src/pcap/add_scan_result.c \
- inc/scan_result.h inc/scan_config.h inc/scan_type.h
-inc/scan_result.h:
-inc/scan_config.h:
-inc/scan_type.h:
