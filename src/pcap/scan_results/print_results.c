cfe25626b3a299eb0ec52f13c7332f959f5b08c2
Erdogan Cayir <erdogancayir@panteon.games>
2025-04-05 23:52:51 +0300

    print result

--- src/pcap/scan_results/print_results.c
+++ src/pcap/scan_results/print_results.c
@@ -13,7 +13,7 @@ void print_results(t_shared_results *results) {
     pthread_mutex_lock(&results->mutex);
 
     printf("\nOpen Ports:\n");
-    printf("Port  Service        Scan-Type         Conclusion\n");
+    printf("Port  Service Name (if applicable)        Scan-Type         Conclusion\n");
     printf("-----------------------------------------------------\n");
 
     t_scan_result *cur = results->head;
@@ -21,13 +21,13 @@ void print_results(t_shared_results *results) {
         if (strcmp(cur->status, "Open") == 0 || strcmp(cur->status, "Open|Filtered") == 0) {
             struct servent *serv = getservbyport(htons(cur->port), "tcp");
             const char *service_name = serv ? serv->s_name : "Unassigned";
-            printf("%-5d %-14s %-17s %s\n", cur->port, service_name, scan_type_to_str(cur->scan_type), cur->status);
+            printf("%-5d %-36s %-17s %s\n", cur->port, service_name, scan_type_to_str(cur->scan_type), cur->status);
         }
         cur = cur->next;
     }
 
     printf("\nClosed/Filtered/Unfiltered Ports:\n");
-    printf("Port  Service        Scan-Type         Conclusion\n");
+    printf("Port  Service Name (if applicable)        Scan-Type         Conclusion\n");
     printf("-----------------------------------------------------\n");
 
     cur = results->head;
@@ -35,7 +35,7 @@ void print_results(t_shared_results *results) {
         if (strcmp(cur->status, "Open") != 0 && strcmp(cur->status, "Open|Filtered") != 0) {
             struct servent *serv = getservbyport(htons(cur->port), "tcp");
             const char *service_name = serv ? serv->s_name : "Unassigned";
-            printf("%-5d %-14s %-17s %s\n", cur->port, service_name, scan_type_to_str(cur->scan_type), cur->status);
+            printf("%-5d %-36s %-17s %s\n", cur->port, service_name, scan_type_to_str(cur->scan_type), cur->status);
         }
         cur = cur->next;
     }
